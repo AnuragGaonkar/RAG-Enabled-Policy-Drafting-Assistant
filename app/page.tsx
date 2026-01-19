87ef15e78ed916c8db7ed7a03ca384277b03ea7c
ThDarKSouL <anuraggaonkar36@gmail.com>
2026-01-19 15:17:54 +0530

    Second Phase Start

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatInterface } from "@/components/chat-interface"
-
-export default function Home() {
-  return (
-    <main className="h-screen bg-background">
-      <ChatInterface />
-    </main>
-  )
-}
